b98b12a607db328a60e690101ea8955f8b205479
508377malik-assignment-453 <T0508377@my.ntu.ac.uk>
2026-04-13 19:42:42 +0100

    Update fareCalculator.java

--- fareCalculator.java
+++ fareCalculator.java
@@ -1,11 +1,12 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-class FareCalculator {
-
-    public static BigDecimal applyDiscount(BigDecimal baseFare,
-                                           CityRideDataset.PassengerType type) {
-        BigDecimal rate = CityRideDataset.DISCOUNT_RATE.get(type);
+public class FareCalculator
+{
+    public static BigDecimal applyDiscount(BigDecimal baseFare, CityRideDataset.PassengerType type,
+                                           Config config)
+    {
+        BigDecimal rate = config.getDiscountRate(type);
         BigDecimal discount = baseFare.multiply(rate).setScale(2, RoundingMode.HALF_UP);
         return baseFare.subtract(discount);
     }
